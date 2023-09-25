6d1b21e09fb33768233241aa8a6c203dd7ef562c
furukoshi12 <syochikubai12@gmail.com>
2023-09-25 23:19:02 +0900

    追加：ルーティング illustrated_bookを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
-      post '/signup', to: 'authentications#signup'
-      post '/login', to: 'authentications#login'
+      post '/signup', to: 'users#create'
+      post '/login', to: 'authentications#create'
       delete '/logout', to: 'authentications#logout'
       post '/guest_login', to: 'authentications#guest_login'
 
       resources :users, only: %i[index show update destroy]
+      resources :illustrated_books, only: %i[index show create update destroy]
     end
   end
 end
