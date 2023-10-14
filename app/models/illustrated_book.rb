8cee953bc909e80dd1031fb1228b78d18d8246ac
furukoshi12 <syochikubai12@gmail.com>
2023-10-14 23:08:04 +0900

    追加：お気に入り機能 LikeモデルのUserとIllustratedBookモデルとのアソシエーション定義

--- app/models/illustrated_book.rb
+++ app/models/illustrated_book.rb
@@ -1,3 +1,4 @@
 class IllustratedBook < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :api_keys, dependent: :destroy
   has_many :illustrated_books, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_illustrated_books, through: :likes, source: :illustrated_book
 
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[crypted_password] }
@@ -12,5 +14,17 @@ class User < ApplicationRecord
   def activate_api_key!
     return api_keys.active.first if api_keys.active.exists?
     api_keys.create
+  end
+
+  def like(illustrated_book)
+    like_illustrated_books << illustrated_book
+  end
+
+  def unlike(illustrated_book)
+    like_illustrated_books.destroy(illustrated_book)
+  end
+
+  def like?(illustrated_book)
+    like_illustrated_books.include?(illustrated_book)
   end
 end
