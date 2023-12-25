2ade7feaec1ca6421132688702c32432450d5978
furukoshi12 <syochikubai12@gmail.com>
2023-12-25 11:19:03 +0900

    追加 User::IllustratedBooksController： ストロングパラメーターでillustrated_book_field_designの入力を許可 IllustratedBookモデル： illustrated_book_field_designをネスト IllustratedBookFieldDesignSerializer： レスポンスにcontentを追加

--- app/serializers/illustrated_book_serializer.rb
+++ app/serializers/illustrated_book_serializer.rb
@@ -4,6 +4,7 @@ class IllustratedBookSerializer
   set_key_transform :camel_lower
   
   belongs_to :user
+  has_many :illustrated_book_field_designs, serializer: IllustratedBookFieldDesignSerializer
 
   attribute :tags do |illustrated_book, params|
     illustrated_book.tags.map { |tag| tag.name }
