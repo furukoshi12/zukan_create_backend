1e69ad7b800aa76bae53c2282a71be137cf5434a
furukoshi12 <syochikubai12@gmail.com>
2024-01-13 05:05:20 +0900

    fix_carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,15 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_directory  = 'zukan-create'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/zukan-create'
   else
