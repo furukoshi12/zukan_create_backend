24b192a538b06433dc3a5f663a6eb9ffde195671
furukoshi12 <syochikubai12@gmail.com>
2024-01-13 03:00:17 +0900

    追加 本番環境でcarrierwaveの画像をアップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  config.asset_host = ENV.fetch('ASSET_HOST', 'http://localhost:3001')
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'zukan-create'
+    config.asset_host = ENV.fetch('ASSET_HOST', 'https://s3-ap-northeast-1.amazonaws.com/zukan-create')
+  else
+    config.asset_host = ENV.fetch('ASSET_HOST', 'http://localhost:3001')
+    config.storage = :file
+    config.cache_storage = :file
+  end
 end
